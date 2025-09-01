6536ada72bdb208ed09535274452f9b7030c179d
anujr <anujrajput8959@gmail.com>
2025-09-01 23:02:33 +0530

    01-09-2025 Update

---
+++ src/test/java/com/AutomateWithAnuj/Lab01_RA.java
@@ -0,0 +1,15 @@
+package com.AutomateWithAnuj;
+
+import io.restassured.RestAssured;
+
+public class Lab01_RA {
+    public static void main(String[] args) {
+        RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com/ping") //this is the URL we are going to use
+                .when()
+                    .get() //in when we need to write what method we have to use
+                    //we can use here .post, .put, .patch, .delete
+                .then()
+                    .log().all().statusCode(201);
+    }
+}
