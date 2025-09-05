edf7540e68fe0f3692146d1e77ee8c0bacb5d19b
anujr <anujrajput8959@gmail.com>
2025-09-05 16:04:50 +0530

    05-09-2025 Update

--- src/test/java/com/APIAutomation/_03_TestNG_AllureReport/ApiTest06_RunMulTestCases.java
+++ src/test/java/com/APIAutomation/_03_TestNG_AllureReport/ApiTest06_RunMulTestCases.java
@@ -4,30 +4,50 @@ import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
 public class ApiTest06_RunMulTestCases {
-    public void runGetRequest(String pincode, int expectedStatus){
+
+    String pincode = "110048";
+
+    @Test
+    public void test_GET_POSITIVE_TC1() {
         RestAssured
                 .given()
-                    .baseUri("https://api.zippopotam.us")
-                    .basePath("/IN/" + pincode)
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
                 .when()
-                    .get()
+                .get()
                 .then()
-                    .log().all()
-                    .statusCode(expectedStatus);
+                .log().all()
+                .statusCode(200);
     }
 
     @Test
-    public void test_GET_POSITIVE_TC1(){
-        runGetRequest("110048", 200);
+    public void test_GET_NEGATIVE_TC2() {
+        pincode = "@";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test
-    public void test_GET_NEGATIVE_TC2(){
-        runGetRequest("@", 404);
-    }
+    public void test_GET_NEGATIVE_TC3() {
+        pincode = " ";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(200);
 
-    @Test
-    public void test_GET_NEGATIVE_TC3(){
-        runGetRequest(" ", 404);
-    }        //but here we are pasting same things all the time and we must have to see something to reuse
+        // but here we are pasting same things all the time
+        // and we must have to see something to reuse
+    }
 }
---
+++ src/test/java/com/APIAutomation/_03_TestNG_AllureReport/ApiTest07_RunMulTestCases.java
@@ -0,0 +1,34 @@
+package com.APIAutomation._03_TestNG_AllureReport;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class ApiTest07_RunMulTestCases {
+    public void runGetRequest(String pincode, int expectedStatus){
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pincode)
+                .when()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(expectedStatus);
+    }
+
+    @Test
+    public void test_GET_POSITIVE_TC1(){
+        runGetRequest("110048", 200);
+    }
+
+    @Test
+    public void test_GET_NEGATIVE_TC2(){
+        runGetRequest("@", 404);
+    }
+
+    @Test
+    public void test_GET_NEGATIVE_TC3(){
+        runGetRequest(" ", 404);
+    }
+    //but here we are pasting same things all the time and we must have to see something to reuse
+}
