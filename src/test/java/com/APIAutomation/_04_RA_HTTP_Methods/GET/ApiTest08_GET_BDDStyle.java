c566ef5ebc76c9a33bf8a6c339e862e571f85832
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-02-10 07:31:32 +0530

    Update ApiTest08_GET_BDDStyle.java

--- src/test/java/com/APIAutomation/_04_RA_HTTP_Methods/GET/ApiTest08_GET_BDDStyle.java
+++ src/test/java/com/APIAutomation/_04_RA_HTTP_Methods/GET/ApiTest08_GET_BDDStyle.java
@@ -3,19 +3,34 @@ package com.APIAutomation._04_RA_HTTP_Methods.GET;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
+/**
+ * This class demonstrates a GET API test
+ * written using Rest Assured BDD (Behaviour Driven Development) style.
+ */
 public class ApiTest08_GET_BDDStyle {
+
+    /**
+     * Test method for positive GET request scenario.
+     * Scenario: A valid Indian pincode is provided.
+     */
     @Test
-    public void test_GET_BDD_Positive_TC1(){
+    public void test_GET_BDD_Positive_TC1() {
+
+        // Test data: valid Indian pincode
         String pincode = "475673";
+
+        // BDD-style API test using Rest Assured
         RestAssured
-                .given()
-                    .baseUri("https://api.zippopotam.us/")
-                    .basePath("/IN/"+pincode)
-                .when()
-                    .log().all()
-                    .get()
-                .then()
-                    .log().all()
-                    .statusCode(200);
+                .given() // GIVEN: request preconditions (URI, headers, params)
+                    .baseUri("https://api.zippopotam.us/") // Base URL of the API
+                    .basePath("/IN/" + pincode) // Endpoint with dynamic pincode
+                .when() // WHEN: the request is executed
+                    .log().all() // Logs request details to console
+                    // Console Output: Request URI, headers, and method
+                    .get() // Executes HTTP GET request
+                .then() // THEN: validate the response
+                    .log().all() // Logs response details to console
+                    // Console Output: Response body, headers, and status
+                    .statusCode(200); // Assertion: HTTP status code should be 200
     }
 }
