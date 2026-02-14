260d6d949966d493bc2ead8ff3929c6e91fec424
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-02-14 15:50:16 +0530

    Enhance ApiTest15 with method documentation
    Added documentation to explain the difference between a normal Java method and a TestNG runnable test case.

--- src/test/java/com/APIAutomation/_05_TestNG_Examples/ApiTest15_RunnableTC_vs_NormalFunction.java
+++ src/test/java/com/APIAutomation/_05_TestNG_Examples/ApiTest15_RunnableTC_vs_NormalFunction.java
@@ -2,14 +2,41 @@ package com.APIAutomation._05_TestNG_Examples;
 
 import org.testng.annotations.Test;
 
+/**
+ * This class demonstrates the difference between:
+ * 1) A normal Java method
+ * 2) A TestNG runnable test case
+ *
+ * Key Concept:
+ * Only methods annotated with @Test will be executed by TestNG.
+ */
 public class ApiTest15_RunnableTC_vs_NormalFunction {
-    //this will not be recognized as this is a runnable test case
-    //this is a normal function
-    public void test_post_request1(){
 
+    /**
+     * This is a NORMAL Java method.
+     * 
+     * ❌ It will NOT be executed by TestNG
+     * because it does not have the @Test annotation.
+     * 
+     * It behaves like any regular method in Java.
+     */
+    public void testPostRequest1() {
+        System.out.println("This is a normal method, NOT executed by TestNG.");
+        // Output: This is a normal method, NOT executed by TestNG.
     }
-    @Test //this will be recognized as this is a runnable test case
-    public void test_post_request2(){
 
+    /**
+     * This is a TestNG test method.
+     * 
+     * ✅ It WILL be executed by TestNG
+     * because it is annotated with @Test.
+     * 
+     * TestNG scans for @Test annotations
+     * and runs only those methods.
+     */
+    @Test
+    public void testPostRequest2() {
+        System.out.println("This is a TestNG runnable test case.");
+        // Output: This is a TestNG runnable test case.
     }
 }
