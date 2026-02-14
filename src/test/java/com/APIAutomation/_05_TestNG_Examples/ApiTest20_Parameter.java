96a7ed429520809b8378e527580c298ce0aa3326
Anuj Rajput <68214879+AutomateWithAnuj@users.noreply.github.com>
2026-02-14 20:55:46 +0530

    Update ApiTest20_Parameter.java

--- src/test/java/com/APIAutomation/_05_TestNG_Examples/ApiTest20_Parameter.java
+++ src/test/java/com/APIAutomation/_05_TestNG_Examples/ApiTest20_Parameter.java
@@ -3,18 +3,45 @@ package com.APIAutomation._05_TestNG_Examples;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+/**
+ * This class demonstrates how to use @Parameters in TestNG.
+ *
+ * Purpose:
+ * - To pass external values (from testng.xml) into test methods.
+ * - Helps in running the same test with different inputs.
+ *
+ * Example:
+ * <parameter name="browser" value="chrome"/>
+ */
 public class ApiTest20_Parameter {
+
+    /**
+     * @Parameters("browser")
+     * - Reads the value of "browser" from testng.xml.
+     * - That value is injected into this method as a String.
+     */
     @Parameters("browser")
     @Test
-    public void demo1(String browser){
-        System.out.println("Hi I am demo");
-        System.out.println("You are running this parameter");
+    public void demoTest(String browser) {
 
-        if(browser.equalsIgnoreCase("firefox")){
+        System.out.println("Hi I am demo"); 
+        // Output: Hi I am demo
+
+        System.out.println("You are running this parameter");
+        // Output: You are running this parameter
+
+        // Check if browser value is "firefox" (case-insensitive)
+        if (browser.equalsIgnoreCase("firefox")) {
+
             System.out.println("Start the firefox");
+            // Output (if parameter = firefox): Start the firefox
         }
-        if(browser.equalsIgnoreCase("chrome")){
+
+        // Check if browser value is "chrome" (case-insensitive)
+        if (browser.equalsIgnoreCase("chrome")) {
+
             System.out.println("Start the chrome");
+            // Output (if parameter = chrome): Start the chrome
         }
     }
 }
